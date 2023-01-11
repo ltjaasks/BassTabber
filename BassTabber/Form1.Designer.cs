3174b4540e159fda6991b370161ff21ce422d342
ltjaasks <lauri.t.jaaskelainen@gmail.com>
2023-01-12 00:51:04 +0200

    modified tab writing

--- BassTabber/Form1.Designer.cs
+++ BassTabber/Form1.Designer.cs
@@ -36,37 +36,42 @@
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tempoBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(355, 9);
+            this.label1.Location = new System.Drawing.Point(406, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 172);
+            this.textBox2.Location = new System.Drawing.Point(14, 229);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 23);
+            this.textBox2.Size = new System.Drawing.Size(137, 27);
             this.textBox2.TabIndex = 2;
             // 
             // Tab
             // 
-            this.Tab.Location = new System.Drawing.Point(148, 72);
+            this.Tab.Location = new System.Drawing.Point(169, 96);
+            this.Tab.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Tab.Name = "Tab";
-            this.Tab.Size = new System.Drawing.Size(640, 337);
+            this.Tab.Size = new System.Drawing.Size(1028, 448);
             this.Tab.TabIndex = 3;
             this.Tab.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(381, 415);
+            this.button1.Location = new System.Drawing.Point(435, 553);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,9 +79,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(481, 415);
+            this.button2.Location = new System.Drawing.Point(550, 553);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(86, 31);
             this.button2.TabIndex = 5;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -85,10 +91,11 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(12, 72);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(14, 96);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.Size = new System.Drawing.Size(137, 124);
             this.listBox1.TabIndex = 7;
             // 
             // timer1
@@ -96,17 +103,37 @@
             this.timer1.Interval = 250;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // tempoBox
+            // 
+            this.tempoBox.Location = new System.Drawing.Point(169, 62);
+            this.tempoBox.Name = "tempoBox";
+            this.tempoBox.Size = new System.Drawing.Size(125, 27);
+            this.tempoBox.TabIndex = 8;
+            this.tempoBox.TextChanged += new System.EventHandler(this.tempoBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1209, 600);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tempoBox);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Tab);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -123,5 +150,7 @@
         private Button button2;
         private ListBox listBox1;
         private System.Windows.Forms.Timer timer1;
+        private TextBox tempoBox;
+        private Label label2;
     }
 }
--- BassTabber/Form1.cs
+++ BassTabber/Form1.cs
@@ -15,12 +15,22 @@ namespace BassTabber
         readonly int BitDepth = 16;
         readonly int ChannelCount = 1;
         readonly int BufferMilliseconds = 50;
+        int Tempo;
+        int Isku;
 
-        double PiikkiTaajuus;
+        int PiikkiTaajuus;
 
         StringBuilder[] TabSb = new StringBuilder[4];
+        List<String> Tahdit = new List<string>();
 
-
+        int[][] oteLauta = new int[][]
+        {
+            new int[] {98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311},
+            new int[] {73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247},
+            new int[] {55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196},
+            new int[] {41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156}
+        };
+         
 
         public Form1()
         {
@@ -37,7 +47,6 @@ namespace BassTabber
 
             double FftPeriod = FftSharp.Transform.FFTfreqPeriod(SampleRate, FftMag.Length);
 
-
             ///Alustetaan ‰‰nilaite input
             AaniIn = new NAudio.Wave.WaveInEvent
             {
@@ -48,9 +57,14 @@ namespace BassTabber
 
             ///Kutsutaan tapahtumank‰sittelij‰‰ AaniIn_DataAvailable kun uutta dataa ‰‰nilaitteesta
             AaniIn.DataAvailable += AaniIn_DataAvailable;
-            
-
         }
+
+
+        private int GetTempo()
+        {
+            return Tempo;
+        }
+
 
         /*
          * Lis‰‰ ‰‰nilaitteet listboxiin TO:DO k‰ytt‰j‰n valittavaksi
@@ -84,7 +98,7 @@ namespace BassTabber
         }
 
 
-        private double AaniTaajuus()
+        private int AaniTaajuus()
         {
             var window = new FftSharp.Windows.Hamming();
             double[] PaddedAudio = FftSharp.Pad.ZeroPad(AudioAanitys);
@@ -106,8 +120,9 @@ namespace BassTabber
             double FftPeriod = FftSharp.Transform.FFTfreqPeriod(SampleRate, FftMag.Length);
             double PiikkiTaajuus = FftPeriod * Piikki;
 
-            return PiikkiTaajuus;
+            return Convert.ToInt32(PiikkiTaajuus);
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -126,14 +141,84 @@ namespace BassTabber
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Text = $"{PiikkiTaajuus:N0}";
-            for (int i = 0; i < TabSb.Length; i++)
-                TabSb[i].Append("ó");
+            Kirjoita();
+        }
+
+        /// <summary>
+        /// Etsii taulukosta oteLauta ‰‰nilaitteen taajuutta vastaavan v‰lin
+        /// </summary>
+        /// <param name="taajuus">ƒ‰nilaitteen poimima taajuus</param>
+        /// <returns>
+        /// Taajuutta vastaava v‰li tai -1
+        /// </returns>
+        private (int, int) EtsiVali(int taajuus)
+        {
+            for (int i = 0; i < oteLauta.Length; i++)
+            {
+                for (int j = oteLauta[i].Length - 1; j >= 0; j--)
+                {
+                    if (taajuus == oteLauta[i][j]) return (i, j);
+                    if (taajuus > oteLauta[i][j])
+                    {
+                        return (i, j);
+                    }
+                }
+            }
+            return (-1, -1);
+        }
+
+
+        private void Kirjoita()
+        {
+            var Aani = (-1, -1);
+            Isku++;
+            if (PiikkiTaajuus != 0)
+                Aani = EtsiVali(PiikkiTaajuus);
+
             
+            for (int i = 0; i < TabSb.Length; i++)
+            {
+                if (i == Aani.Item1)
+                {
+                    TabSb[Aani.Item1].Append(Aani.Item2 + "ó");
+                } else
+                {
+                    TabSb[i].Append("óó");
+                }
+
+                if (Isku == 32)
+                    TabSb[i].Append("|");
+            }
+
+            if (Isku == 32)
+            {
+                StringBuilder Sb = new StringBuilder("");
+                for (int i = 0; i < TabSb.Length; i++)
+                {
+                    Sb.Append(TabSb[i].ToString() + "\n");
+                    TabSb[i].Clear();
+                }
+                Tahdit.Add(Sb.ToString());
+                Isku = 0;
+            }
+
             Tab.Clear();
+
+            for (int i = 0; i < Tahdit.Count; i++)
+                Tab.AppendText(Tahdit[i] + "\n");
             for (int i = 0; i < TabSb.Length; i++)
                 Tab.AppendText(TabSb[i].ToString() + "\n");
         }
 
 
+        private void tempoBox_TextChanged(object sender, EventArgs e)
+        {
+            int.TryParse(tempoBox.Text, out Tempo);
+            label2.Text = $"{Tempo}bpm";
+            if (Tempo != 0)
+            {
+                timer1.Interval = 15000 / Tempo;
+            }
+        }
     }
 }
